509d847b3bed8a7f93db77d844d5efe0030cac10
Matheus Alves <159180480+Maziin07@users.noreply.github.com>
2024-03-14 12:52:43 -0300

    Add files via upload

---
+++ agendador.bat
@@ -0,0 +1,10 @@
+@echo off
+cls
+@echo.
+@echo *********************************
+@echo ***                           ***
+@echo ***  ESTOU EXECUTANDO UM .BAT ***
+@echo ***                           ***
+@echo *********************************
+@echo.
+pause
